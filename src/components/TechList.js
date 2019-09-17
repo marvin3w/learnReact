90b18cc1a4937c2779524bff7b20613d7c7bc95e
marvin3w <marvin.3w@gmail.com>
2019-09-17 08:13:19 -0300

    Remove state

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -20,12 +20,22 @@ class TechList extends Component{
     this.setState({ techs: [...this.state.techs, this.state.newTech], newTech: '' })
   }
 
+  handleDelete = (tech) => {
+    this.setState({ techs: this.state.techs.filter( t => t !== tech ) });
+  }
+
   render() {
     return (
       <>
         <form onSubmit={this.handleSubmit}>
           <ul>
-            {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
+            {this.state.techs.map(tech =>(
+              <li key={tech}>
+                {tech}
+                <button onClick={() => this.handleDelete(tech)} type="button">remover</button>
+              </li>
+            ) 
+            )}
           </ul>
           <input type="text" onChange={this.handleInputChange} value={this.state.newTech} />
           <button type="submit">Enviar</button>
